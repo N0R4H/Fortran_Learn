4db5b60faa1b8ad312290f23684ff12f9de73f8b
N0R4H <rohanpan27@gmail.com>
2022-02-16 09:26:39 +0530

    initial commit

---
+++ programaddNumbers.f90
@@ -0,0 +1,17 @@
+!my first fortran program POG
+
+program addNumbers
+
+! This simple program adds two numbers
+   implicit none
+
+! Type declarations
+   real :: a, b, result
+
+! Executable statements
+   a = 121.0
+   b = 15.0
+   result = a + b
+   print *, 'The total is ', result
+
+end program addNumbers
