670d1ee368d62f964f835b04fb136845868157ed
N0R4H <rohanpan27@gmail.com>
2022-03-12 21:29:52 +0530

    UPdated statements

--- fortranlearn.f90
+++ fortranlearn.f90
@@ -1,7 +1,7 @@
 
 program fortranlean
 
-	REAL::area, radius
+	REAL ::area, radius
 	REAL, PARAMETER :: PI = 3.141592656869
 
 	radius = 43.12
@@ -9,5 +9,12 @@ program fortranlean
 	area = pi *radius*radius
 	!WRITE (6, *) area
 	print *, "Area is ", area
+
+	!If else conditional statement
+	IF (area > 5)THEN
+		print *, "bIG"
+	ElSE
+		print *, "Small"
+	END IF
 
 end program fortranlean
